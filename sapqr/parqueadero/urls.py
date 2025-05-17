08800f62019549c13695bbeb7c65f15f833ca88c
dannyaguilargil <danny.aguilar.gil@gmail.com>
2025-05-17 12:57:59 -0500

    casita

--- sapqr/parqueadero/urls.py
+++ sapqr/parqueadero/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.registro_general, name='casita'),
     path('registro/', views.registro_general, name='registro_general'),
     path('registro/<uuid:uuid>/', views.registro_qr, name='registro_qr'),
 ]
